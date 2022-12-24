44b1e98e2528a835143565ca3d2c850ed159b254
sajkop123 <sajkop1234@gmail.com>
2022-12-24 22:36:26 +0800

    Update TypeEasure2.cpp

--- TypeEasure2.cpp
+++ TypeEasure2.cpp
@@ -8,25 +8,38 @@
 #include <atomic>
 #include <cassert>
 
-struct TypeErasure {
-  template<typename T>
-  TypeErasure(T&& value) { *this = value; }
+/* Something about type erasure*/
+// 1. not template
+// 2. templated constructor
+// 3. has `Concept` class, the type erased object and we hold the pointer
+// 4. templated `Model` class inherits `Concept`
 
+struct TypeErasure {
+  // template constructor/operator, instead template struct.
+  template<typename T>
+  TypeErasure(T&& value) {
+    *this = value;  // forward to operator= overloading
+  }
   template<typename T>
   TypeErasure& operator=(T&& value) {
     m_value.reset(new Model<T>(value));
     return *this;
   }
   
+  // I want my template object to have same common interface.
   void printName() const {
+    // the impl of printName() on type erased.
+    // it's going to forward on a function on the same name of `Concept`
     m_value->printName();
   }
 
  private:
+  // base class for all these specilizations of `Model`
   struct Concept {
     virtual ~Concept() {}
     virtual void printName() const = 0;
   };
+  // 'Model` is responsible to know the actual type it holds.
   template<typename T>
   struct Model : Concept {
     Model(const T& value) : m_val(value) {}
