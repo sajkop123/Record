258bd6468ced48c0b82595bf4112a7ef3b34eda7
sajkop123 <sajkop1234@gmail.com>
2023-09-10 21:30:46 +0800

    Update member_function_result_cache.cpp

--- member_function_result_cache.cpp
+++ member_function_result_cache.cpp
@@ -1,6 +1,11 @@
 #include <functional>
 #include <iostream>
 #include <map>
+#include <memory>
+#include <atomic>
+#include <mutex>
+#include <thread>
+
 using namespace std;
 
 template <typename T>
@@ -22,6 +27,17 @@ struct function_traits<R (C::*)(Args...)> {
   static constexpr size_t arg_count = sizeof...(Args); 
 };
 
+// partial specilization for const member function pointer
+template<class C, typename R, typename ...Args>
+struct function_traits<R (C::*)(Args...) const> {
+  using return_type = R;
+  using class_type = C;
+  using function_type = R (C::*)(Args...) const;
+  using arg_tuple_type = std::tuple<Args...>;
+  static constexpr size_t arg_count = sizeof...(Args); 
+};
+
+
 // helper
 template<typename T>
 using getReturnType = typename function_traits<T>::return_type;
@@ -29,15 +45,22 @@ template<typename T>
 using getClassType = typename function_traits<T>::class_type;
 template<typename T>
 using getArgTupleType = typename function_traits<T>::arg_tuple_type;
-template<typename T>
-using getFnType = typename function_traits<T>::function_type;
+
+template<typename ArgTuple_T, typename Return_T>
+struct MemFnResCache {
+  
+};
 
 template<typename MemFuncPtr_T>
 struct MemFnCache {
   using Return_T = getReturnType<MemFuncPtr_T>;
   using Class_T = getClassType<MemFuncPtr_T>;
   using ArgTuple_T = getArgTupleType<MemFuncPtr_T>;
-  using Fn_T = getFnType<MemFuncPtr_T>;
+
+  struct Result_T {
+    Return_T mReturn;
+    std::atomic_bool isReady = false;  // default false to confirm one write
+  };
 
   MemFnCache() {}
   MemFnCache(Class_T* pObj, MemFuncPtr_T pMemFn) :
@@ -45,8 +68,9 @@ struct MemFnCache {
 
   template<typename ...Args>
   Return_T execute(Args&&... args) {
-    std::cout << "user called\n";
-    if (mResult.count(std::forward_as_tuple(args...)) <= 0) {
+    // std::cout << "user called\n";
+    std::unique_lock<std::mutex> _l(mMutex);
+    if(mResult.count(std::forward_as_tuple(args...)) <= 0) {
       mResult[std::forward_as_tuple(args...)] =
           (mpObj->*mpMemFn)(std::forward<Args>(args)...);
     }
@@ -57,6 +81,7 @@ struct MemFnCache {
   Class_T* mpObj;
   MemFuncPtr_T mpMemFn;
   mutable std::map<ArgTuple_T, Return_T> mResult;
+  mutable std::mutex mMutex;
 };
 
 
@@ -81,6 +106,7 @@ struct ResultMemento {
 struct Usage : ResultMemento<Usage> {
   bool test_impl(int a, int b) override {
     std::cout << "impl" << "\n";
+    called_times++;
     return a == 0 && b == 0;
   }
 
@@ -90,15 +116,39 @@ struct Usage : ResultMemento<Usage> {
   bool test2(int a, int b) const {
     return a == 0 && b == 0;
   }
+
+  int called_times = 0;
 };
 
 int main() {
   Usage usage;
   bool b;
-  b = usage.test(0, 10);
-  b = usage.test(0, 10);
-  b = usage.test(1, 10);
-  b = usage.test(0, 10);
+  // b = usage.test(0, 10);
+  // b = usage.test(0, 10);
+  // b = usage.test(1, 10);
+  // b = usage.test(0, 10);
+
+  const static size_t SIZE = 20;
+  const static size_t ITERATION = 5;
+  std::thread st[SIZE];
+  for (size_t i = 0; i < SIZE; ++i) {
+    st[i] = (
+      std::thread([obj = &usage]() {
+        for (size_t j = 0; j < ITERATION; ++j) {
+          obj->test(0 ,10);
+        }
+      }
+    ));
+  }
+
+  for (size_t i = 0; i < SIZE; ++i) {
+    st[i].join();
+  }
+
+
+  if (usage.called_times > 1) {
+    std::cout << "GGGGGGGGGGGGGGGGGGG" << "\n";
+  }
 
   return 1;
 }
